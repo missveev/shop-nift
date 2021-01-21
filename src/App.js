73686e8af7edfa627a7fd3d57de056e584c3b4c3
missveev <vivianpamilerin@gmail.com>
2021-01-21 21:08:45 +0100

    react app project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import Homepage from "./Pages/homepage/Homepage";
 import ShopPage from "./Pages/shop/shop";
-
+import Header from "./Component/header/header";
 function App() {
   return (
     <div>
+        <Header/>
         <Switch>
         <Route  exact path='/' component={Homepage} />
         <Route  path='/shop' component={ShopPage} />
