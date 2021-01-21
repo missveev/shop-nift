55588ed7e3b012e603b3613a03a31b90998cfa14
missveev <vivianpamilerin@gmail.com>
2021-01-21 19:56:45 +0100

    react app project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import Homepage from "./Pages/homepage/Homepage";
-
-
+const HatsPage =() => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
 function App() {
   return (
-    <div className="App">
-<Homepage/>
-    </div>
+    <div>
+        <Switch>
+        <Route  exact path='/' component={Homepage} />
+        <Route  path='/hats' component={HatsPage} />
+        </Switch>
+        </div>
   );
 }
 
