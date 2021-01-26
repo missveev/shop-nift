1d59653b1781db6d9b10fdb3e52acfda8e9b02d7
missveev <vivianpamilerin@gmail.com>
2021-01-26 22:05:15 +0100

    added carticon and cartdropdown to app

---
+++ src/Component/cart-dropdown/cart-dropdown.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CustomButton from "../custom-button/customButton";
+import './cart-dropdown.scss';
+const CartDropdown =()=>(
+    <div className='cart-dropdown'>
+        <div className='cart-items'/>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+)
+export default CartDropdown;
